eb4b841b74a26c8880ea000f703219165ced2bc8
Volodymyr Dzhupyna <vdzhupyna@gmail.com>
2022-10-08 21:08:25 +0300

    added task1 from sprint01

---
+++ sprint01/1.py
@@ -0,0 +1,11 @@
+def kthTerm(n, k):
+    num_list = [0, 1]
+    counter = 1
+    while len(num_list) < k:
+        for item in range(len(num_list)):
+            num_list.append(n**counter + num_list[item])
+        counter += 1
+    return num_list
+
+
+print(kthTerm(3, 4))
