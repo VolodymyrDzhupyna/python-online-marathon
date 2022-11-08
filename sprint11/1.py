43ad245f819d508a7d7401dfd0a770f63798f55f
Volodymyr Dzhupyna <vdzhupyna@gmail.com>
2022-11-08 15:06:42 +0200

    added close() and Base.metadata.create_all(engine) fixed get_info

--- sprint11/1.py
+++ sprint11/1.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///sprint11/q1.db")
+engine = create_engine("sqlite:///sprint11/q1.db", echo=False)
+print("Connected to SQLite")
+
 Base = declarative_base()
 
 
@@ -15,35 +17,27 @@ class Customer(Base):
     grade = Column(Integer)
     salesperson_id = Column(Integer)
 
-    def __init__(self, name, city, grade, salesperson_id):
-        self.name = name
-        self.city = city
-        self.grade = grade
-        self.salesperson_id = salesperson_id
-    
-    def __repr__(self):
-        return f"{self.id} {self.name} {self.city} {self.grade} {self.salesperson_id}"
-
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-query = session.query(Customer).filter(Customer.grade > 200).order_by(Customer.id)
-clients = query.all()
-
-number_records = query.count()
-
-
 def get_info():
-    print("Connected to SQLite")
+    query = session.query(Customer).filter(Customer.grade > 200).order_by(Customer.id)
+    clients = query.all()
+    number_records = query.count()
+    column_names = Customer.__table__.columns.keys()
     print("Total rows are:  ", number_records)
     print("Printing each row")
     for client in clients:
-        print(f"Id: ", client.id)
-        print("Name: ", client.name)
-        print("City: ", client.city)
-        print("Grade: ", client.grade)
-        print("Seller: ", client.salesperson_id, end="\n\n\n")
+        for item in column_names:
+            if item == "salesperson_id":
+                print(f"Seller:  {getattr(client, item)}")
+            else:
+                print(f"{item.capitalize()}:  {getattr(client, item)}")
+        print(end="\n\n")
     print("The SQLite connection is closed")
+
+session.close()
 
 
 if __name__ == "__main__":
